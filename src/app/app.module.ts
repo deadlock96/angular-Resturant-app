bd36c2dab5809286b45ae64f91182f8d06df3a85
deadlock96 <akshay.hansinfotech@gmail.com>
2022-03-05 16:16:54 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule} from '@angular/common/http'
 import { ResturantDashComponent } from './resturant-dash/resturant-dash.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResturantDashComponent
+    ResturantDashComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
