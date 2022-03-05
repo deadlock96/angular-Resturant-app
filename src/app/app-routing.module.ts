41c5c5b45e242efeea733dd05a39d5cc6b11a400
deadlock96 <akshay.hansinfotech@gmail.com>
2022-03-05 16:16:31 +0530

    app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ResturantDashComponent } from './resturant-dash/resturant-dash.component';
+import { SignUpComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '', 
+    redirectTo: 'login', 
+    pathMatch:'full'
+  },{
+    path: 'login', 
+    component:LoginComponent
+  },{
+    path: 'signup', 
+    component: SignUpComponent
+  },{
+    path: 'resturant-dash', 
+    component: ResturantDashComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
